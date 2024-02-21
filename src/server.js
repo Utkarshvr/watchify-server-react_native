8e9f197a7d6810f9b3311dde1378b8698d134ea1
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2024-02-22 00:14:33 +0530

    lax & secure: false (hope so)

--- src/server.js
+++ src/server.js
@@ -40,9 +40,9 @@ app.use(
     }),
     cookie: {
       maxAge: 24 * 60 * 60 * 1000 * 7,
-      sameSite: "none", // Allow cross-site cookies
-      secure: true, // Only send cookies over HTTPS
-      // httpOnly: true, // Prevent client-side script access
+      sameSite: "lax", // Allow cross-site cookies
+      secure: false, // Only send cookies over HTTPS
+      httpOnly: true, // Prevent client-side script access
     },
   })
 );
